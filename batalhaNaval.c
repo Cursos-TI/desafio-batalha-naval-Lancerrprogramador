b6f2515d14f9e9e312b675e78a299e012bb4bfc9
Lancerrprogramador <cauanjonny@gmail.com>
2025-04-25 01:05:37 +0000

    navio

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,55 +4,72 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 // DESAFIO FEITO POR JONNY LIMA
+#include <stdio.h>
+
 int main() {
- 
-        //Criando o tabuleiro
-        int Tabuleiro[10][10];
-        char coluna [10] = {'1','2','3','4','5','6','7','8','9','10'};  
-        char linha [10] = {'A','B','C','D','E','F','G','H','I','J'};
-        int i, j;
-    
-        // Preenche o tabuleiro com 0
-        for(i = 0; i < 10; i++){
-            for(j = 0; j < 10; j++){
-                Tabuleiro[i][j] = 0;
+    int Tabuleiro[10][10];
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    char coluna[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10'};
+    int i, j;
+    int linha_inicial_num, coluna_inicial_num;
+    char linha_inicial_char;
+    char orientacao;
+    int valido = 1;
+
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            Tabuleiro[i][j] = 0;
+        }
+    }
+
+    linha_inicial_char = 'B';
+    coluna_inicial_num = 3;
+    orientacao = 'H';
+
+    linha_inicial_num = linha_inicial_char - 'A';
+    coluna_inicial_num--;
+
+    if (orientacao == 'H') {
+        if (coluna_inicial_num > 7) {
+            valido = 0;
+            printf("Posição inválida: Navio horizontal excede o limite do tabuleiro.\n");
+        } else {
+            for (j = coluna_inicial_num; j < coluna_inicial_num + 3; j++) {
+                Tabuleiro[linha_inicial_num][j] = 3;
             }
-        }   
-        // Imprime o tabuleiro
-        printf("  ");   
-        for(i = 0; i < 10; i++){
+        }
+    } else if (orientacao == 'V') {
+        if (linha_inicial_num > 7) {
+            valido = 0;
+            printf("Posição inválida: Navio vertical excede o limite do tabuleiro.\n");
+        } else {
+            for (i = linha_inicial_num; i < linha_inicial_num + 3; i++) {
+                Tabuleiro[i][coluna_inicial_num] = 3;
+            }
+        }
+    } else {
+        valido = 0;
+        printf("Orientação inválida. Use 'H' para horizontal ou 'V' para vertical.\n");
+    }
+
+    if (valido) {
+        printf("  ");
+        for (i = 0; i < 10; i++) {
             printf("%c ", coluna[i]);
         }
-        printf("\n");   
-    
-        for(i = 0; i < 10; i++){
+        printf("\n");
+        for (i = 0; i < 10; i++) {
             printf("%c ", linha[i]);
-            for(j = 0; j < 10; j++){
+            for (j = 0; j < 10; j++) {
                 printf("%d ", Tabuleiro[i][j]);
             }
-            printf("\n");   
+            printf("\n");
         }
-     
+    }
 
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+     
+
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
@@ -85,4 +102,4 @@ int main() {
     // 0 0 1 0 0
 
     return 0;
-}
+    }
